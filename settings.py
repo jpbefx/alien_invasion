3b229bff79e89ad5219cb1b106d1efdc778fa270
James jpb <jpbefx@hotmail.com>
2024-01-11 23:47:45 -0600

    Created 'settings.py' file
    Added basic screen settings to start with.

---
+++ settings.py
@@ -0,0 +1,12 @@
+# James Bailey January 11, 2024
+#   Chapter 12 'Python Crash Course' textbook Pg 231.
+
+class Settings:
+    """A class to store all settings for Alien Invasion."""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+        #   Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
