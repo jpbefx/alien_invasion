11d6e0023f377bc034ca57b61ecb00d37f6d3c1f
James jpb <jpbefx@hotmail.com>
2024-01-11 23:23:19 -0600

    Created 'alien_invasion.py' file
    Starting the code for game window and background.

---
+++ alien_invasion.py
@@ -0,0 +1,20 @@
+# James Bailey January 11, 2024
+#   Chapter 12 'Python Crash Course' textbook Pg 229.
+
+import sys
+
+import pygame
+
+class AlienInvasion:
+    """Overal class to manage game assets and behavior."""
+
+    def __init__(self):
+        """Initialize the game, and create game resources."""
+
+    def run_game(self) -> None:
+        pass
+
+    if __name__ == '__main__':
+        #   Make a game instance, and run the game.
+        ai = AlienInvasion()
+        ai.run_game()
