f6225fe0b07d00649d325aedd8b89bd8b04163d1
James jpb <jpbefx@hotmail.com>
2024-01-12 22:48:25 -0600

    Updated 'alien_invasion.py'
    Added call to Ship(self), also added call to ship.blitme().

--- alien_invasion.py
+++ alien_invasion.py
@@ -23,6 +23,8 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
         
     def run_game(self):
         """Start the main loop for the game."""
@@ -34,6 +36,7 @@ class AlienInvasion:
 
             #   Redraw the screen during each pass through the loop. (pg 231)
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             #   Make the most recently drawn screen visible.
             pygame.display.flip()
